1b05aefb1d22aa8e322cd1bf1436e4ded8411a45
Thomas Barton <verites95@gmail.com>
2020-06-03 15:06:42 +0100

    HelloWorld2 in Netbeans

---
+++ HelloWorld2/src/main/java/Hello2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tombarton
+ */
+package tb95.HelloWorld;
+
+public class Hello2 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+    
+}
+
